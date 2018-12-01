f46411810187d47408362a271d58aa7c6522231f
code-bandit <ripudamansingh1807@gmail.com>
2018-12-01 17:08:31 +0530

    new code

--- picamBarcode.py
+++ picamBarcode.py
@@ -1,35 +1,29 @@
-import cv2
 from time import sleep
 from picamera import PiCamera
 from io import BytesIO
 from pyzbar.pyzbar import decode
-from PIL import Video
+from PIL import Image
 
-def FrameCapture(path): 
-    vidObj = cv2.VideoCapture(path) 
-    count = 0
-    success = 1
-  
-    while success: 
-        success, image = vidObj.read() 
-	decoded_image = decode(image)
-	if decoded_image:
-		print(str(count) + " " + decoded_image)
-		return 1
-	count += 1
+class BarCode:
+    def __init__(self):
+        camera = PiCamera()
+    def read():
+        for i in range(30):
+            stream = BytesIO()
+            camera.capture(stream, format="jpg")
+            stream.seek(0)
+            image = Image.open(stream)
+            decoded_image = decode(image)
+            if decoded_image:
+                return decoded_image
+                break
+            sleep(1)
+        return -1
+    def close():
+        camera.close()
+        del(camera)
 
-if __name__ == '__main__': 
-    FrameCapture("home\\pi\\Desktop\\tklab\\barcode\\opencv\\openCV.mp4") 
-
-#cam = cv2.VideoCapture(0)
-camera = PiCamera()
-stream = BytesIO()
-camera.start_recording("home/pi/Desktop/tklab/barcode/opencv/test.h264")
-camera.wait_recording(1)
-camera.stop_recording()
-d = FrameCapture("home/pi/Desktop/tklab/barcode/opencv/test.h264")
-# image = Image.open(stream)
-# decoded_image = decode(image)
-print(d)
-
-del(camera)
+BarCode bar()
+result = bar.read()
+print(result)
+bar.close()
